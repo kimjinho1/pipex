a72a262f5f02c51ae19b5319c63937a7e781be3d
kimjinho1 <rlawlsgh8113@naver.com>
2022-10-30 05:29:42 +0900

    mandatory modify

--- mandatory/pipex.c
+++ mandatory/pipex.c
@@ -19,13 +19,13 @@ void	child_process(char **av, char **envp, int *fd)
 	infile = open(av[1], O_RDONLY);
 	if (infile == -1)
 		perror_exit("infile error");
-	close(fd[0]);
 	if (dup2(infile, STDIN_FILENO) == -1)
 		perror_exit("dup2 error");
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
 		perror_exit("dup2 error");
-	close(fd[1]);
 	close(infile);
+	close(fd[0]);
+	close(fd[1]);
 	execute(av[2], envp);
 }
 
@@ -36,46 +36,34 @@ void	parent_process(char **av, char **envp, int *fd)
 	outfile = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (outfile == -1)
 		perror_exit("outfile error");
-	close(fd[1]);
 	if (dup2(fd[0], STDIN_FILENO) == -1)
 		perror_exit("dup2 error");
 	if (dup2(outfile, STDOUT_FILENO) == -1)
 		perror_exit("dup2 error");
-	close(fd[0]);
 	close(outfile);
+	close(fd[0]);
+	close(fd[1]);
 	execute(av[3], envp);
-}
-
-void	wait_all_child(pid_t *pid_li, int n)
-{
-	int	i;
-	int	status;
-
-	i = 0;
-	while (i < n)
-	{
-		if (waitpid(pid_li[i], &status, 0))
-			i++;
-	}
 }
 
 int	main(int ac, char **av, char **envp)
 {
-	pid_t	pid_li[2];
+	pid_t	pid;
 	int		fd[2];
 
 	if (ac != 5)
 		perror_exit("argument error");
-	pid_li[0] = fork();
-	if (pid_li[0] == 0)
+	if (pipe(fd) == -1)
+		perror_exit("pipe error");
+	pid = fork();
+	if (pid == -1)
+		perror_exit("fork error");
+	if (pid == 0)
+		child_process(av, envp, fd);
+	else
 	{
-		if (pipe(fd) == -1)
-			perror_exit("pipe error");
-		pid_li[1] = fork();
-		if (pid_li[1] == 0)
-			child_process(av, envp, fd);
-		else
-			parent_process(av, envp, fd);
+		parent_process(av, envp, fd);
+		waitpid(pid, NULL, 0);
 	}
-	wait_all_child(pid_li, 2);
+	return (0);
 }
